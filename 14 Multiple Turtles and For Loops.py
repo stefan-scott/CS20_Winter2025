c43f5e4f66e338713f98bc48a9a983543729a2b2
stefan-scott <scottst@spsd.sk.ca>
2025-05-01 10:14:47 -0600

    wip

---
+++ 14 Multiple Turtles and For Loops.py
@@ -0,0 +1,22 @@
+# Multiple Turtles and For Loops
+# Mr. Scott
+# May 1st, 2025
+
+# Turtle Setup Code (Boilerplate)
+import turtle #loads turtle.py
+
+# Create Screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.setup(700,700)
+
+# Create multiple turtles
+turtle_list = []
+color_list = ["red", "green", "blue", "orange", "yellow", "grey", "white"]
+
+NUM_TURTLES = 3 #all caps → Constant
+
+
+
+# At the very end
+wn.exitonclick()
