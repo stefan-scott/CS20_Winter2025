600ffb1597b87c06412505b1164f1bea5fee2842
stefan-scott <scottst@spsd.sk.ca>
2025-05-12 11:12:59 -0600

    Update 17b L-System Demo.py

--- 17b L-System Demo.py
+++ 17b L-System Demo.py
@@ -3,16 +3,25 @@
 # May 12, 2025
 
 # Turtle Code Setup Here...
+import turtle
+t = turtle.Turtle()
+wn = turtle.Screen()
+wn.setup(700,700)
 
+t.speed(0)
+#set initial position
+t.up()
+t.goto(-200,0)
+t.down()
 
 # L-System Code Here...
 def apply_rules(ch):
     # Apply our L-System rules to a single character
     # We'll modify this for different L-systems...
-    if ch == "A":
-        return "B"
-    elif ch == "B":
-        return "AB"
+    if ch == "L":
+        return "+R-F-R+"
+    elif ch == "R":
+        return "-L+F+L-"
     else:  #If there is no rule to match ch
         return ch
 
@@ -35,4 +44,26 @@ def create_L_system(num_generations, axiom):
         
     return next_generation
     
+# Time to drive the turtle...
+def draw_L_system(instructions, angle, distance):
+    # interpret our L-system with the Turtle
+    for cmd in instructions:
+        if cmd == "F":
+            t.fd(distance)
+        elif cmd == "B":
+            t.bk(distance)
+        elif cmd == "+":
+            t.right(angle)
+        elif cmd == "-":
+            t.left(angle)
+        turtle.update() #animate quickly!
+    
+# MAIN CODE HERE
+turtle.tracer(False)
+commands = create_L_system(14, "L--F--L--F")
+print(commands)
+draw_L_system(commands, 37.5, 1)
 
+
+wn.exitonclick()
+    
